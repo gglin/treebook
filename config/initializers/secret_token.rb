52579e949c772a084e4586920c40dab963c6c0e2
gglin <george.g.lin@gmail.com>
2013-05-31 18:14:55 -0400

    First commit with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '219ea63e52ae380c1d10d9bb8f644cc1353bf246629ca9eb0d65fb0c2236989a71fa8c0ef2c530a1f1e323a82be4c3c9890b34c4588a1d08bff6b2e38e13b573'
